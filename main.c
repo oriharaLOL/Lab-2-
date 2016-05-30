9069970edc3232b295aca2f771ef6724da3b4bbc
Jake <JUk@localhost.localdomain>
2016-05-30 19:19:48 +0600

    изменение main.c

--- main.c
+++ main.c
@@ -1,5 +1,18 @@
 #include<stdio.h>
 #include<math.h>
+dic(float a,float b,float c)
+{
+float x1,x2,D;
+D=b*b-4*a*c;
+if(D<0)
+{printf("no roots");}
+if(D>=0)
+{
+x1= (-b + sqrt(D))/(2*a);
+x2= (-b - sqrt(D))/(2*a);
+printf("x1 = %g , x2 = %g",x1,x2);
+}
+}
 
 int main()
 { float a,b,c;
